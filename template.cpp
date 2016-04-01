c72d88e571b179e4d5b8db189960c533537f8fb3
dipta007 <iamdipta@gmail.com>
2016-04-01 09:55:52 +0600

    update some files

--- template.cpp
+++ template.cpp
@@ -25,34 +25,26 @@
 #include <vector>
 using namespace std;
 
-
-
 const double EPS = 1e-9;
 const int INF = 0x7f7f7f7f;
 const double PI=acos(-1.0);
 
 #define    READ(f) 	         freopen(f, "r", stdin)
 #define    WRITE(f)   	     freopen(f, "w", stdout)
-
 #define    MP(x, y) 	     make_pair(x, y)
-#define    SZ(c) 	         (int)c.size()
 #define    PB(x)             push_back(x)
-
-#define    rep(i,n)          for(i=1;i<=n;i++)
-#define    repI(i,n)         for(i=0;i<n;i++)
-#define    F(i,L,R)	         for (int i = L; i < R; i++)
-#define    FF(i,L,R) 	     for (int i = L; i <= R; i++)
-#define    FR(i,L,R) 	     for (int i = L; i > R; i--)
-#define    FRF(i,L,R) 	     for (int i = L; i >= R; i--)
+#define    rep(i,n)          for(int i = 1 ; i<=(n) ; i++)
+#define    repI(i,n)         for(int i = 0 ; i<(n) ; i++)
+#define    FOR0(i,L,R)	     for (int i = L; i < R; i++)
+#define    FOR1(i,L,R) 	     for (int i = L; i <= R; i++)
+#define    ROF0(i,L,R) 	     for (int i = L; i > R; i--)
+#define    ROF1(i,L,R) 	     for (int i = L; i >= R; i--)
 #define    FOREACH(i,t)      for (typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
 #define    ALL(p) 	         p.begin(),p.end()
 #define    ALLR(p) 	         p.rbegin(),p.rend()
-
 #define    SET(p) 	         memset(p, -1, sizeof(p))
 #define    CLR(p)            memset(p, 0, sizeof(p))
 #define    MEM(p, v)         memset(p, v, sizeof(p))
-#define    CPY(d, s)         memcpy(d, s, sizeof(s))
-
 #define    getI(a) 	         scanf("%d", &a)
 #define    getII(a,b) 	     scanf("%d%d", &a, &b)
 #define    getIII(a,b,c)     scanf("%d%d%d", &a, &b, &c)
@@ -62,19 +54,17 @@ const double PI=acos(-1.0);
 #define    getC(n)           scanf("%c",&n)
 #define    getF(n)           scanf("%lf",&n)
 #define    getS(n)           scanf("%s",n)
-
+#define    bitCheck(a,k)     ((bool)(a&(1<<(k))))
+#define    bitOff(a,k)       (a&(~(1<<(k))))
+#define    bitOn(a,k)         (a|(1<<(k)))
+#define    iseq(a,b)          (fabs(a-b)<EPS)
 #define    vi 	 vector < int >
 #define    vii 	 vector < vector < int > >
 #define    pii 	 pair< int, int >
-#define    psi 	 pair< string, int >
 #define    ff 	 first
 #define    ss 	 second
 #define    ll	 long long
 #define    ull 	 unsigned long long
-#define    ui    unsigned int
-#define    us 	 unsigned short
-#define    ld 	 long double
-
 
 template< class T > inline T _abs(T n) { return ((n) < 0 ? -(n) : (n)); }
 template< class T > inline T _max(T a, T b) { return (!((a)<(b))?(a):(b)); }
@@ -84,8 +74,6 @@ template< class T > inline T gcd(T a, T b) { return (b) == 0 ? (a) : gcd((b), ((
 template< class T > inline T lcm(T a, T b) { return ((a) / gcd((a), (b)) * (b)); }
 template <typename T> string NumberToString ( T Number ) { ostringstream ss; ss << Number; return ss.str(); }
 
-//******************DELETE****************
-#define shubha
 #ifdef dipta007
      #define debug(args...) {cerr<<"Debug: "; dbg,args; cerr<<endl;}
 #else
@@ -99,68 +87,14 @@ struct debugger{
     }
 }dbg;
 
-int bitOn(int N,int pos)
-{
-    return N=N | (1<<pos);
-}
-int bitOff(int N,int pos)
-{
-    return N=N & ~(1<<pos);
-}
-bool bitCheck(int N,int pos)
-{
-    return (bool)(N & (1<<pos));
-}
-
-vector <pii> vp;
-
 int main() {
     #ifdef dipta007
-        READ("in.txt");
-        WRITE("out.txt");
-    #endif
+        //READ("in.txt");
+        //WRITE("out.txt");
+    #endif // dipta007
 
-    int t;
-    getI(t);
-    for(int ci=1;ci<=t;ci++)
-    {
-        vp.clear();
-        int p,c;
-        getII(p,c);
-        for(int i=0;i<p;i++)
-        {
-            int x;
-            getI(x);
-            vp.PB(pii(x,1));
-        }
-        for(int i=0;i<c;i++)
-        {
-            int x;
-            getI(x);
-            vp.PB(pii(x+1,-1));
-        }
-        sort(ALL(vp));
-        int money=0,lmoney=0;
-        int angry=p,langry=p;
-        for(int i=0;i<vp.size();)
-        {
-//            debug(vp[i].ff,vp[i].ss)
-            int val = vp[i].ff;
-            while(i<vp.size() && val==vp[i].ff)
-            {
-                if(vp[i].ss == 1) angry--;
-                else angry++;
-                i++;
-            }
-            money=val;
-            if(angry<langry)
-            {
-                lmoney=money;
-                langry=angry;
-            }
-        }
-        printf("%d %d\n",lmoney,langry);
-    }
+
+
 
     return 0;
 }
