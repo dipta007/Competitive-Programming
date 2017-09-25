ef5a3cdde867b098e1f8d490c9aad11ef16aedda
Shubhashis Roy Dipta <iamdipta@gmail.com>
2017-09-25 20:41:16 +0600

    added codeforces-manthan & kickstart-F-2017

---
+++ kickstart/F - 2017/A.cpp
@@ -0,0 +1,208 @@
+#pragma comment(linker, "/stack:640000000")
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+const double EPS = 1e-9;
+const int INF = 0x7f7f7f7f;
+const double PI=acos(-1.0);
+
+#define    READ(f) 	         freopen(f, "r", stdin)
+#define    WRITE(f)   	     freopen(f, "w", stdout)
+#define    MP(x, y) 	     make_pair(x, y)
+#define    PB(x)             push_back(x)
+#define    rep(i,n)          for(int i = 1 ; i<=(n) ; i++)
+#define    repI(i,n)         for(int i = 0 ; i<(n) ; i++)
+#define    FOR(i,L,R) 	     for (int i = (int)(L); i <= (int)(R); i++)
+#define    ROF(i,L,R) 	     for (int i = (int)(L); i >= (int)(R); i--)
+#define    FOREACH(i,t)      for (typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+#define    ALL(p) 	         p.begin(),p.end()
+#define    ALLR(p) 	         p.rbegin(),p.rend()
+#define    SET(p) 	         memset(p, -1, sizeof(p))
+#define    CLR(p)            memset(p, 0, sizeof(p))
+#define    MEM(p, v)         memset(p, v, sizeof(p))
+#define    getI(a) 	         scanf("%d", &a)
+#define    getII(a,b) 	     scanf("%d%d", &a, &b)
+#define    getIII(a,b,c)     scanf("%d%d%d", &a, &b, &c)
+#define    getL(a)           scanf("%lld",&a)
+#define    getLL(a,b)        scanf("%lld%lld",&a,&b)
+#define    getLLL(a,b,c)     scanf("%lld%lld%lld",&a,&b,&c)
+#define    getC(n)           scanf("%c",&n)
+#define    getF(n)           scanf("%lf",&n)
+#define    getS(n)           scanf("%s",n)
+#define    bitCheck(N,in)    ((bool)(N&(1<<(in))))
+#define    bitOff(N,in)      (N&(~(1<<(in))))
+#define    bitOn(N,in)       (N|(1<<(in)))
+#define    bitCount(a)        __builtin_popcount(a)
+#define    iseq(a,b)          (fabs(a-b)<EPS)
+#define    UNIQUE(V) (V).erase(unique((V).begin(),(V).end()),(V).end())
+#define    vi 	 vector < int >
+#define    vii 	 vector < vector < int > >
+#define    pii 	 pair< int, int >
+#define    ff 	 first
+#define    ss 	 second
+#define    ll	 long long
+#define    ull 	 unsigned long long
+
+template< class T > inline T _abs(T n) { return ((n) < 0 ? -(n) : (n)); }
+template< class T > inline T _max(T a, T b) { return (!((a)<(b))?(a):(b)); }
+template< class T > inline T _min(T a, T b) { return (((a)<(b))?(a):(b)); }
+template< class T > inline T _swap(T &a, T &b) { a=a^b;b=a^b;a=a^b;}
+template< class T > inline T gcd(T a, T b) { return (b) == 0 ? (a) : gcd((b), ((a) % (b))); }
+template< class T > inline T lcm(T a, T b) { return ((a) / gcd((a), (b)) * (b)); }
+template <typename T> string NumberToString ( T Number ) { ostringstream ss; ss << Number; return ss.str(); }
+
+#ifdef dipta00
+     #define debug(args...) {cerr<<"Debug: "; dbg,args; cerr<<endl;}
+#else
+    #define debug(args...)  // Just strip off all debug tokens
+#endif
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cerr<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+#define MAX 10004
+int arr[MAX];
+
+
+int tree[MAX*4];
+
+void init(int node,int b,int e)
+{
+	if(b==e)
+	{
+		tree[node]=1;
+		return;
+	}
+	int Left= node << 1;
+	int Right= (node << 1) +1;
+	int mid=(b+e)>>1;
+	init(Left,b,mid);
+	init(Right,mid+1,e);
+	tree[node]=tree[Left]+tree[Right];
+}
+int query(int node,int b,int e,int val)
+{
+//	if (i > e || j < b)	return 0;
+	if (b == e) return b;
+	int Left=node<<1;
+	int Right=(node<<1)+1;
+	int mid=(b+e)>>1;
+	if(tree[Left] >= val)
+    {
+        return query(Left,b,mid,val);
+    }
+    else
+    {
+        val -= tree[Left];
+        return query(Right,mid+1,e,val);
+    }
+}
+
+void update(int node,int b,int e,int i,int newvalue=0)
+{
+	if (i > e || i < b)	return;
+	if (b >= i && e <= i) {
+		tree[node]=newvalue;
+		return;
+	}
+	int Left=node<<1;
+	int Right=(node<<1)+1;
+	int mid=(b+e)>>1;
+	update(Left,b,mid,i,newvalue);
+	update(Right,mid+1,e,i,newvalue);
+	tree[node]=tree[Left]+tree[Right];
+}
+
+int main() {
+    #ifdef dipta007
+        READ("in.txt");
+        WRITE("out.txt");
+    #endif // dipta007
+//    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    int t;
+    getI(t);
+    FOR(ci,1,t)
+    {
+        int n;
+        getI(n);
+        init(1,0,n-1);
+
+        vi v,sv;
+        FOR(i,1,n)
+        {
+            int x;
+            getI(x);
+            v.PB(x);
+            sv.PB(x);
+        }
+
+        sort(ALL(sv));
+
+        int l = 1, r= n;
+
+        vi q;
+        ROF(i,n,1)
+        {
+            int kk = (i-1);
+            q.PB(floor((double)kk/2.0));
+        }
+
+        int flg = 0;
+
+        FOR(i,0,n-1)
+        {
+            int kk = query(1, 0, n-1, q[i]+1);
+            debug(q[i], kk, v[kk])
+            if(v[kk]==l)
+            {
+                l++;
+            }
+            else if(v[kk]==r)
+            {
+                r--;
+            }
+            else
+            {
+                flg = 1;
+                break;
+            }
+            update(1,0,n-1,kk);
+        }
+        printf("Case #%d: ",ci);
+        if(flg) printf("NO\n");
+        else printf("YES\n");
+    }
+
+    return 0;
+}
+
+
+
