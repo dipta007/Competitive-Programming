cba3c543dd17596a486df1b69dd1823ad53df4d3
Shubhashis Roy <iamdipta@gmail.com>
2020-06-12 12:10:16 +0600

    added edu 89-2

---
+++ Codeforces/edu_89-2/D.py
@@ -0,0 +1,150 @@
+""" Python 3 compatibility tools. """
+from __future__ import division, print_function
+import itertools
+import sys
+import os
+from io import BytesIO, IOBase
+
+
+if sys.version_info[0] < 3:
+  input = raw_input
+  range = xrange
+
+  filter = itertools.ifilter
+  map = itertools.imap
+  zip = itertools.izip
+
+
+def is_it_local():
+  script_dir = str(os.getcwd()).split('/')
+  username = "dipta007"
+  return username in script_dir
+
+
+def READ(fileName):
+  if is_it_local():
+    sys.stdin = open(f'./{fileName}', 'r')
+
+# region fastio
+BUFSIZE = 8192
+
+class FastIO(IOBase):
+  newlines = 0
+
+  def __init__(self, file):
+    self._fd = file.fileno()
+    self.buffer = BytesIO()
+    self.writable = "x" in file.mode or "r" not in file.mode
+    self.write = self.buffer.write if self.writable else None
+
+  def read(self):
+    while True:
+      b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+      if not b:
+          break
+      ptr = self.buffer.tell()
+      self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+    self.newlines = 0
+    return self.buffer.read()
+
+  def readline(self):
+    while self.newlines == 0:
+      b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+      self.newlines = b.count(b"\n") + (not b)
+      ptr = self.buffer.tell()
+      self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+    self.newlines -= 1
+    return self.buffer.readline()
+
+  def flush(self):
+    if self.writable:
+      os.write(self._fd, self.buffer.getvalue())
+      self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+  def __init__(self, file):
+    self.buffer = FastIO(file)
+    self.flush = self.buffer.flush
+    self.writable = self.buffer.writable
+    self.write = lambda s: self.buffer.write(s.encode("ascii"))
+    self.read = lambda: self.buffer.read().decode("ascii")
+    self.readline = lambda: self.buffer.readline().decode("ascii")
+
+if not is_it_local():
+  sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+  input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+
+def input1(type=int):
+  return type(input())
+
+
+def input2(type=int):
+  [a, b] = list(map(type, input().split()))
+  return a, b
+
+
+def input3(type=int):
+  [a, b, c] = list(map(type, input().split()))
+  return a, b, c
+
+
+def input_array(type=int):
+  return list(map(type, input().split()))
+
+
+def input_string():
+  s = input()
+  return list(s)
+
+##############################################################
+
+MX = 10000002
+odd1 = [-1 for _ in range(MX)]
+import math
+
+def generate():
+  global MX, odd1
+  sq = int(math.sqrt(MX))
+  for i in range(3, MX, 2):
+    if odd1[i] == -1:
+      for j in range(i, MX, i):
+        if odd1[j] == -1:
+          odd1[j] = i
+    # print(i)
+
+
+
+def main():
+  generate()
+  n = input1()
+  ar = input_array()
+
+  r1, r2 = [], []
+  for v in ar:
+    # print(v, odd[v])
+    if v%2 == 0: # even
+      p = 2
+    else:
+      p = odd1[v]
+    
+    while v % p == 0:
+      v //= p
+    
+    q = v
+    if v == 1:
+      p = -1
+      q = -1
+    
+    r1.append(p)
+    r2.append(q)
+  print(" ".join([str(x) for x in r1]))
+  print(" ".join([str(x) for x in r2]))
+  pass
+
+if __name__ == '__main__':
+  READ('in.txt')
+  main()
